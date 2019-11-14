aea4689209ef78b1e8047f118cba57aeff37d1a0
Odd Eirik Igland <oddeirik.igland@gmail.com>
2019-11-14 11:04:45 +0000

    looks for sources

--- modules/program_analysis.py
+++ modules/program_analysis.py
@@ -1,7 +1,10 @@
+variable_condition_store = {}
+
+
 class Node:
     def __init__(self, value):
         self.value = value
-        self.interesting_stuff =[]
+        self.interesting_stuff = []
         self.children = []
 
     def add_child(self, child_node):
@@ -11,7 +14,9 @@ class Node:
         self.interesting_stuff.append(interest)
 
     def __str__(self, level=0):
-        ret = "\t" * level + repr(self.value) + ": " + repr(self.interesting_stuff) + "\n"
+        ret = (
+            "\t" * level + repr(self.value) + ": " + repr(self.interesting_stuff) + "\n"
+        )
         for child in self.children:
             ret += child.__str__(level + 1)
         return ret
@@ -30,16 +35,34 @@ def find_interesting_values(d, node):
         node.add_interest(d["id"])
 
 
-def walk_dict(d, parent_node):
+def check_if_tainted(d, node, sources):
+    for variable in d["targets"]:
+        variable_condition_store[variable["id"]] = True
+
+    if d["value"]["ast_type"] == "Call":
+        try:
+            if d["value"]["func"]["id"] in sources:
+                print(d["value"]["func"]["id"])
+                # Todo: find the id of the variable, to save it for the output
+        except KeyError:
+            # Todo: which functions do not have an id?
+            # Todo: dont think we need to consider them
+            print("something else")
+    print(variable_condition_store)
+
+
+def walk_dict(d, parent_node, sources):
     node = Node(d["ast_type"])
     find_interesting_values(d, node)
+    if d["ast_type"] == "Assign":
+        check_if_tainted(d, node, sources)
     parent_node.add_child(node)
     for key, value in d.items():
         if isinstance(value, dict):
-            walk_dict(value, node)
+            walk_dict(value, node, sources)
         if isinstance(value, list):
             for elem in value:
-                walk_dict(elem, node)
+                walk_dict(elem, node, sources)
 
 
 def walk_interesting_tree(node, to_find):
@@ -54,18 +77,13 @@ def walk_interesting_tree(node, to_find):
 
 
 def program_analysis(program_slice_json, vuln_pattern_json):
-    template = {
-        "vulnerability": "",
-        "source": "",
-        "sink": "",
-        "sanitizer": "",
-    }
+    template = {"vulnerability": "", "source": "", "sink": "", "sanitizer": ""}
     output = []
     pattern = vuln_pattern_json[0]
 
     top_node = Node(program_slice_json["ast_type"])
     for operations in program_slice_json["body"]:
-        walk_dict(operations, parent_node=top_node)
+        walk_dict(operations, parent_node=top_node, sources=pattern["sources"])
 
     print(top_node)
     sink = walk_interesting_tree(top_node, to_find=pattern["sinks"])
