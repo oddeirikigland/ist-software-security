aa0603a05f26d631d53a669fd7b173b3a3562a1e
Odd Eirik Igland <oddeirik.igland@gmail.com>
2019-11-20 14:51:09 +0000

    adds check for sanitizers

--- modules/program_analysis.py
+++ modules/program_analysis.py
@@ -1,5 +1,6 @@
 tainted_dict = {}
 parent_dict = {}
+sanitized_dict = {}
 output = []
 template = {"vulnerability": "", "source": "", "sink": "", "sanitizer": ""}
 
@@ -11,12 +12,13 @@ STATUS = "status"
 UNIQUE_KEY = "123321"
 
 
-def determine_level(d, sources):
-    variable_to_be_assign = d["targets"][0]["id"]
-    tainted_dict[variable_to_be_assign] = {STATUS: check_if_tainted(d["value"], sources, variable_to_be_assign)}
+def determine_level(d, sources, sanitizers, variable_to_be_assign):
+    tainted_dict[variable_to_be_assign] = {
+        STATUS: check_if_tainted(d["value"], sources, sanitizers, variable_to_be_assign)
+    }
 
 
-def check_if_tainted(d, sources, variable_to_be_assign):
+def check_if_tainted(d, sources, sanitizers, variable_to_be_assign):
     if d["ast_type"] == "Num" or d["ast_type"] == "Str":
         return NOT_TAINTED
 
@@ -35,30 +37,42 @@ def check_if_tainted(d, sources, variable_to_be_assign):
 
     if d["ast_type"] == "Call":
         if d["func"]["ast_type"] == "Attribute":
-            source = d["func"]["attr"]
+            function_name = d["func"]["attr"]
         else:
-            source = d["func"]["id"]
-
-        if source in sources:
+            function_name = d["func"]["id"]
+        if function_name in sources:
             return TAINTED
+
+        if function_name in sanitizers:
+            status = SANITIZED
+        else:
+            status = NOT_TAINTED
         for arg in d["args"]:
-            return check_if_tainted(arg, sources, variable_to_be_assign)
-        return NOT_TAINTED
+            current_status = check_if_tainted(
+                arg, sources, sanitizers, variable_to_be_assign
+            )
+            if current_status == TAINTED and status == SANITIZED:
+                sanitized_dict[arg["id"]] = function_name
+            if status == NOT_TAINTED:
+                status = current_status
+        return status
 
     if d["ast_type"] == "BinOp":
-        left = check_if_tainted(d["left"], sources, variable_to_be_assign)
-        right = check_if_tainted(d["right"], sources, variable_to_be_assign)
+        left = check_if_tainted(d["left"], sources, sanitizers, variable_to_be_assign)
+        right = check_if_tainted(d["right"], sources, sanitizers, variable_to_be_assign)
         return TAINTED if left == TAINTED or right == TAINTED else NOT_TAINTED
 
     if d["ast_type"] == "Attribute":
-        return check_if_tainted(d["value"], sources, variable_to_be_assign)
+        return check_if_tainted(d["value"], sources, sanitizers, variable_to_be_assign)
     raise RuntimeError("ALARM! Unconsidered type")
 
 
-def walk_dict(d, sources, sinks):
+def walk_dict(d, sources, sanitizers, sinks):
     ast_type = d["ast_type"]
     if ast_type == "Assign":
-        determine_level(d, sources)
+        determine_level(
+            d, sources, sanitizers, variable_to_be_assign=d["targets"][0]["id"]
+        )
 
     if ast_type == "Call":
         if d["func"]["ast_type"] == "Attribute":
@@ -68,19 +82,25 @@ def walk_dict(d, sources, sinks):
         if sink in sinks:
             for arg in d["args"]:
                 sink_key = sink + UNIQUE_KEY
-                status = check_if_tainted(arg, sources, variable_to_be_assign=sink_key)
-                if status == TAINTED:
+                status = check_if_tainted(
+                    arg, sources, sanitizers, variable_to_be_assign=sink_key
+                )
+                if status == TAINTED or status == SANITIZED:
                     new_template = template
                     new_template["sink"] = sink
                     new_template["source"] = parent_dict[sink_key]
+                    if status == SANITIZED:
+                        new_template["sanitizer"] = sanitized_dict[
+                            parent_dict[sink_key]
+                        ]
                     output.append(new_template)
 
     for key, value in d.items():
         if isinstance(value, dict):
-            walk_dict(value, sources, sinks)
+            walk_dict(value, sources, sanitizers, sinks)
         if isinstance(value, list):
             for elem in value:
-                walk_dict(elem, sources, sinks)
+                walk_dict(elem, sources, sanitizers, sinks)
 
 
 def program_analysis(program_slice_json, vuln_pattern_json):
@@ -89,7 +109,12 @@ def program_analysis(program_slice_json, vuln_pattern_json):
 
     for pattern in vuln_pattern_json:
         for operations in program_slice_json["body"]:
-            walk_dict(operations, sources=pattern["sources"], sinks=pattern["sinks"])
+            walk_dict(
+                operations,
+                sources=pattern["sources"],
+                sanitizers=pattern["sanitizers"],
+                sinks=pattern["sinks"],
+            )
         for vul in output:
             vul["vulnerability"] = pattern["vulnerability"]
         if len(output) > 0:
