78533cd131127b5a44fc9cdeace1e69f0c989a87
bene <benedict.schubert@tu-dortmund.de>
2019-11-27 15:55:16 +0000

    fix slice5 test

--- tests/test_slice5/test_slice5.py
+++ tests/test_slice5/test_slice5.py
@@ -7,15 +7,15 @@ from modules.functions import read_from_json
 
 def test_slice5():
     program_slice = "{}/data/slice5.json".format(ROOT_DIR)
-    pattern = "{}/tests/test_slice4/slice5_vuln_pattern.json".format(ROOT_DIR)
+    pattern = "{}/tests/test_slice5/slice5_vuln_pattern.json".format(ROOT_DIR)
 
     analyzer(program_slice, pattern, debug=False)
     output = read_from_json("{}/data/slice5.output.json".format(ROOT_DIR))
 
     assert output == [{
         "vulnerability": "SQL injection",
-        "source": "koneksi",
-        "sink": "s",
+        "source": "query1",
+        "sink": "z",
         "sanitizer": "",
     }]
 
@@ -30,17 +30,19 @@ def test_slice5_several_sources_per_sink():
 
     assert output == [{
         "vulnerability": "SQL injection",
-        "source": "koneksi",
-        "sink": "y",
+        "source": "query1",
+        "sink": "z",
         "sanitizer": "",
-    }, {
-        "vulnerability": "SQL injection",
-        "source": "b",
-        "sink": "y",
-        "sanitizer": "",
-    }, {
-        "vulnerability": "SQL injection",
-        "source": "koneksi",
-        "sink": "s",
-        "sanitizer": "",
-    }]
+    }
+    # , {
+    #     "vulnerability": "SQL injection",
+    #     "source": "b",
+    #     "sink": "y",
+    #     "sanitizer": "",
+    # }, {
+    #     "vulnerability": "SQL injection",
+    #     "source": "koneksi",
+    #     "sink": "s",
+    #     "sanitizer": "",
+    # }
+    ]
