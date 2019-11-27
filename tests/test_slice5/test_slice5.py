0a3e8856d2d91e371827a9fa9cf922da6b5f17a9
bene <benedict.schubert@tu-dortmund.de>
2019-11-27 16:00:26 +0000

    removed second test func for slice 5

--- tests/test_slice5/test_slice5.py
+++ tests/test_slice5/test_slice5.py
@@ -18,31 +18,3 @@ def test_slice5():
         "sink": "z",
         "sanitizer": "",
     }]
-
-
-@pytest.mark.skip(reason="no way of currently testing this")
-def test_slice5_several_sources_per_sink():
-    program_slice = "{}/data/slice5.json".format(ROOT_DIR)
-    pattern = "{}/tests/test_slice5/slice5_vuln_pattern.json".format(ROOT_DIR)
-
-    analyzer(program_slice, pattern, debug=False)
-    output = read_from_json("{}/data/slice5.output.json".format(ROOT_DIR))
-
-    assert output == [{
-        "vulnerability": "SQL injection",
-        "source": "query1",
-        "sink": "z",
-        "sanitizer": "",
-    }
-    # , {
-    #     "vulnerability": "SQL injection",
-    #     "source": "b",
-    #     "sink": "y",
-    #     "sanitizer": "",
-    # }, {
-    #     "vulnerability": "SQL injection",
-    #     "source": "koneksi",
-    #     "sink": "s",
-    #     "sanitizer": "",
-    # }
-    ]
